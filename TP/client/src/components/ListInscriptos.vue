0ff58f256fe16a4653555230f8e2bc2489aeb45b
Gianfranco Raselli <66041020+GianfrancoRaselli@users.noreply.github.com>
2021-06-24 17:18:31 -0300

    guardar admin vacantes

--- TP/client/src/components/ListInscriptos.vue
+++ TP/client/src/components/ListInscriptos.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <div class="inscriptos" v-if="llamado && llamado.postulaciones.length > 0">
+    <div class="inscriptos" v-if="llamado && llamado.postulaciones && llamado.postulaciones.length > 0">
       <div class="mb-2">
-        <button class="btn btn-primary mr-1" @click="calificar" v-if="!editando">Calificar</button>
-        <button class="btn btn-success ml-1" v-if="editando">Guardar cambios</button>
-        <button class="btn btn-danger ml-1" v-if="editando" @click="cancelarEdicion">Cancelar cambios</button>
+        <button class="btn btn-primary mr-1" @click="calificar" v-if="!editando"><i class="fas fa-edit"></i> Calificar</button>
+        <button class="btn btn-success ml-1" v-if="editando" @click="guardar"><i class="fas fa-save"></i> Guardar cambios</button>
+        <button class="btn btn-danger ml-1" v-if="editando" @click="cancelar"><i class="fas fa-times-circle"></i> Cancelar cambios</button>
       </div>
-      <table class="table table-responsive table-striped">
+      <table class="table table-responsive table-striped table-hover table-bordered">
         <thead>
           <tr>
             <th>DNI</th>
@@ -14,50 +14,58 @@
             <th>Email</th>
             <th>Teléfono</th>
             <th>Estado</th>
-            <th>Puntaje</th>
+            <th>Calificación</th>
             <th>Comentarios</th>
             <th>Acción</th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="(postulacion, index) in llamado.postulaciones" :key="index">
-            <td>{{ postulacion.persona.dni }}</td>
-            <td>{{ postulacion.persona.nombre_apellido }}</td>
-            <td>{{ postulacion.persona.email }}</td>
-            <td>{{ postulacion.persona.telefono }}</td>
+            <td><div class="columna-md">{{ postulacion.persona.dni }}</div></td>
+            <td><div class="columna-md">{{ postulacion.persona.nombre_apellido }}</div></td>
+            <td><div class="columna-md">{{ postulacion.persona.email }}</div></td>
+            <td><div class="columna-md">{{ postulacion.persona.telefono }}</div></td>
             <td>
-              <div v-if="!editando">
-                {{ postulacion.estado }}
-              </div>
-              <div v-else>
-                <select name="estado" class="form-control">
-                  <option>Aceptar</option>
-                  <option>Rechazar</option>
-                </select>
+              <div class="columna-md">
+                <div v-if="!editando">
+                  {{ postulacion.estado }}
+                </div>
+                <div v-else>
+                  <select name="estado" class="form-control" :class="{ errorClass: postulacion.estadoError }" v-model="postulacion.estadoEditado">
+                    <option>Aceptar</option>
+                    <option>Rechazar</option>
+                  </select>
+                </div>
               </div>
             </td>
             <td>
-              <div v-if="!editando">
-                <div v-if="postulacion.puntaje">{{ postulacion.puntaje }}</div>
-                <div>-</div>
-              </div>
-              <div v-else>
-                <input type="number" class="form-control" min="0" max="10">
-              </div>
-            </td>
-            <td>
-            <div v-if="!editando">
-                <div v-if="postulacion.comentarios">{{ postulacion.comentarios }}</div>
-                <div>-</div>
-              </div>
-              <div v-else>
-                <textarea class="form-control" cols="60"></textarea>
+              <div class="columna-sm">
+                <div v-if="!editando">
+                  <div v-if="postulacion.puntaje">{{ postulacion.puntaje }}</div>
+                  <div>-</div>
+                </div>
+                <div v-else>
+                  <input type="number" class="form-control" :class="{ errorClass: postulacion.puntajeError }" min="0" max="100" v-model="postulacion.puntajeEditado">
+                </div>
               </div>
             </td>
             <td>
-              <a id="btn-ver-cv" class="btn btn-secondary" v-if="postulacion.persona.curriculum_vitae" :href="'http://localhost/Entornos Graficos/entornos-graficos-2021/TP/server/public/CVs/' + postulacion.persona.curriculum_vitae" target="_blank">
-                <i class="fas fa-eye"></i> Ver CV
-              </a>
+              <div class="columna-lg">
+                <div v-if="!editando">
+                  <div v-if="postulacion.comentarios">{{ postulacion.comentarios }}</div>
+                  <div>-</div>
+                </div>
+                <div v-else>
+                  <textarea class="form-control" cols="60" v-model="postulacion.comentariosEditado"></textarea>
+                </div>
+              </div>
+            </td>
+            <td>
+              <div class="columna-md">
+                <a id="btn-ver-cv" class="btn btn-secondary" v-if="postulacion.persona.curriculum_vitae" :href="'http://localhost/Entornos Graficos/entornos-graficos-2021/TP/server/public/CVs/' + postulacion.persona.curriculum_vitae" target="_blank">
+                  <i class="fas fa-eye"></i> Ver CV
+                </a>
+              </div>
             </td>
           </tr>
         </tbody>
@@ -79,13 +87,46 @@ export default {
     calificar() {
       this.editando = true;
     },
-    cancelarEdicion() {
+    cancelar() {
       this.editando = false;
+    },
+    guardar() {
+      if ((this.$store.getters.isAdministrador || this.$store.getters.isJefeCatedra) 
+      && this.llamado && this.llamado.postulaciones && this.llamado.postulaciones.length > 0) {
+        let error = false;
+        for (let postulacion of this.llamado.postulaciones) {
+          postulacion.estadoError = false;
+          postulacion.puntajeError = false;
+          if (!postulacion.estadoEditado) {
+            postulacion.estadoError = true;
+            error = true;
+          }
+          if (!postulacion.puntajeEditado || postulacion.puntajeEditado < 0 || postulacion.puntajeEditado > 100) {
+            postulacion.puntajeError = true;
+            error = true;
+          }
+          console.log(error);
+        }
+      }
     }
   }
 }
 </script>
 
 <style>
+  .columna-lg {
+    width: 360px;
+  }
 
+  .columna-md {
+    width: 160px;
+  }
+
+  .columna-sm {
+    width: 120px;
+  }
+
+  .errorClass {
+    background-color: rgb(218, 139, 139);
+  }
 </style>
