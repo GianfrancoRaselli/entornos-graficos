29057b1f5bd68f685a9870b2c1f5574e02441fca
Gianfranco Raselli <66041020+GianfrancoRaselli@users.noreply.github.com>
2021-06-25 18:18:22 -0300

    correcion erores inscriptos

--- TP/client/src/components/ListInscriptos.vue
+++ TP/client/src/components/ListInscriptos.vue
@@ -1,69 +1,75 @@
 <template>
   <div>
-    <div class="inscriptos" v-if="llamado && llamado.postulaciones.length > 0">
-      <div class="mb-2 table-options">
-        <button class="btn btn-primary mr-1" @click="calificar" v-if="!editando">Calificar</button>
-        <button class="btn btn-success ml-1" v-if="editando">Guardar cambios</button>
-        <button class="btn btn-danger ml-1" v-if="editando" @click="cancelarEdicion">Cancelar cambios</button>
+    <div class="inscriptos" v-if="llamado && llamado.postulaciones && llamado.postulaciones.length > 0">
+      <div class="mb-2 buttons-nav">
+        <button class="btn btn-primary mr-1" @click="calificar" v-if="!editando"><i class="fas fa-edit"></i> Calificar</button>
+        <button class="btn btn-success ml-1" v-if="editando" @click="guardar"><i class="fas fa-save"></i> Guardar cambios</button>
+        <button class="btn btn-danger ml-1" v-if="editando" @click="cancelar"><i class="fas fa-times-circle"></i> Cancelar cambios</button>
       </div>
-      <div class="inscriptos-table">
-        <table class="table table-responsive table-striped">
-          <thead>
-            <tr>
-              <th>DNI</th>
-              <th>NyA</th>
-              <th>Email</th>
-              <th>Teléfono</th>
-              <th>Estado</th>
-              <th>Puntaje</th>
-              <th>Comentarios</th>
-              <th>Acción</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(postulacion, index) in llamado.postulaciones" :key="index">
-              <td>{{ postulacion.persona.dni }}</td>
-              <td>{{ postulacion.persona.nombre_apellido }}</td>
-              <td>{{ postulacion.persona.email }}</td>
-              <td>{{ postulacion.persona.telefono }}</td>
-              <td>
+      <table class="table table-responsive table-striped table-hover table-bordered">
+        <thead>
+          <tr>
+            <th>DNI</th>
+            <th>Nombre y apellido</th>
+            <th>Email</th>
+            <th>Teléfono</th>
+            <th>Estado</th>
+            <th>Calificación</th>
+            <th>Comentarios</th>
+            <th>Acción</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(postulacion, index) in llamado.postulaciones" :key="index">
+            <td><div class="columna-md">{{ postulacion.persona.dni }}</div></td>
+            <td><div class="columna-md">{{ postulacion.persona.nombre_apellido }}</div></td>
+            <td><div class="columna-md">{{ postulacion.persona.email }}</div></td>
+            <td><div class="columna-md">{{ postulacion.puntajeError }}</div></td>
+            <td>
+              <div class="columna-md">
                 <div v-if="!editando">
                   {{ postulacion.estado }}
                 </div>
                 <div v-else>
-                  <select name="estado" class="form-control">
+                  <select name="estado" class="form-control" :class="{ errorClass: postulacion.estadoError }" v-model="postulacion.estadoEditado">
                     <option>Aceptar</option>
                     <option>Rechazar</option>
                   </select>
                 </div>
-              </td>
-              <td>
+              </div>
+            </td>
+            <td>
+              <div class="columna-sm">
                 <div v-if="!editando">
                   <div v-if="postulacion.puntaje">{{ postulacion.puntaje }}</div>
-                  <div>-</div>
+                  <div v-else>-</div>
                 </div>
                 <div v-else>
-                  <input type="number" class="form-control" min="0" max="10">
+                  <input type="number" class="form-control" :class="{ errorClass: postulacion.puntajeError }" min="0" max="100" v-model="postulacion.puntajeEditado">
                 </div>
-              </td>
-              <td>
-              <div v-if="!editando">
+              </div>
+            </td>
+            <td>
+              <div class="columna-lg">
+                <div v-if="!editando">
                   <div v-if="postulacion.comentarios">{{ postulacion.comentarios }}</div>
-                  <div>-</div>
+                  <div v-else>-</div>
                 </div>
                 <div v-else>
-                  <textarea class="form-control" cols="60"></textarea>
+                  <textarea class="form-control" cols="60" v-model="postulacion.comentariosEditado"></textarea>
                 </div>
-              </td>
-              <td>
+              </div>
+            </td>
+            <td>
+              <div class="columna-md">
                 <a id="btn-ver-cv" class="btn btn-secondary" v-if="postulacion.persona.curriculum_vitae" :href="'http://localhost/Entornos Graficos/entornos-graficos-2021/TP/server/public/CVs/' + postulacion.persona.curriculum_vitae" target="_blank">
                   <i class="fas fa-eye"></i> Ver CV
                 </a>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
     <div v-else>
       <p>No hay personas inscriptas</p>
@@ -74,13 +80,9 @@
 <script>
 import axios from 'axios'
 export default {
-  data() {
-    return {
-      editando: false,
-    }
-  },
   props: {
     llamado: { required: true },
+    editando: { required: true, default: false }
   },
   methods: {
     calificar() {
@@ -100,29 +102,44 @@ export default {
             postulacion.estadoError = true;
             error = true;
           }
-          if (!postulacion.puntajeEditado || isNaN(postulacion.puntajeEditado) || postulacion.puntajeEditado % 1 != 0 || postulacion.puntajeEditado < 0 || postulacion.puntajeEditado > 100) {
+          if (!postulacion.puntajeEditado || isNaN(postulacion.puntajeEditado) || postulacion.puntajeEditado % 1 != 0 || postulacion.puntajeEditado < 1 || postulacion.puntajeEditado > 100) {
             postulacion.puntajeError = true;
             error = true;
           }
         }
-
         if (!error) {
-            try {
-              let res = await axios.post('/llamados/calificarLlamado',
-              {
-                llamado: this.llamado
-              },
-              {
-                headers: {
-                  Authorization: 'Bearer ' + this.$store.getters.user.api_token
-                }
-              });
-              this.llamado = res.data;
-              this.editando = false;
-            } catch (err) {
-              console.log(err.response.data.error);
+          try {
+            let res = await axios.post('/llamados/calificarLlamado',
+            {
+              llamado: this.llamado
+            },
+            {
+              headers: {
+                Authorization: 'Bearer ' + this.$store.getters.user.api_token
+              }
+            });
+
+            this.llamado = res.data;
+            for (let postulacion of this.llamado.postulaciones) {
+              if (postulacion.estado === "Elegido") {
+                postulacion.estadoEditado = "Aceptar";
+              } else if (postulacion.estado === "No elegido") {
+                postulacion.estadoEditado = "Rechazar";
+              } else {
+                postulacion.estadoEditado = postulacion.estado;
+              }
+              postulacion.puntajeEditado = postulacion.puntaje;
+              postulacion.comentariosEditado = postulacion.comentarios;
+              postulacion.estadoError = false;
+              postulacion.puntajeError = false;
+              postulacion.comentariosError = false;
             }
+
+            this.editando = false;
+          } catch (err) {
+            console.log(err.response.data.error);
           }
+        }
       }
     }
   }
@@ -130,15 +147,19 @@ export default {
 </script>
 
 <style>
-.table-options{
-  display: flex;
-  justify-content: center;
-}
-@media(min-width:1200px){
-  .table-responsive{
-    width: 80%;
-    margin-left: auto;
-    margin-right: auto;
+  .columna-lg {
+    width: 360px;
   }
-}
+
+  .columna-md {
+    width: 160px;
+  }
+
+  .columna-sm {
+    width: 120px;
+  }
+
+  .errorClass {
+    background-color: rgb(228, 167, 167);
+  }
 </style>
