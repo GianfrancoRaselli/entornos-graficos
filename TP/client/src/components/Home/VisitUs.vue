fb2158666bc38041a64f70fb8ed9d72a8de4202e
Federico Romaniuk <federomaniuk@gmail.com>
2021-06-26 13:08:32 -0300

    Update home

--- TP/client/src/components/Home/VisitUs.vue
+++ TP/client/src/components/Home/VisitUs.vue
@@ -5,11 +5,21 @@
         Visita nuestras instalaciones
       </h4>
       <p>
-        Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem.
+        
       </p>
+      <div class="mt-5 mb-5 px-5 row">
+        <div class="col-md-6 col-sm-12 mt-3" style="text-align:justify">
+          En Rosario, la Facultad Regional de la Universidad Tecnológica Nacional tiene sus orígenes en 1953 sobre la base de la Escuela Fábrica N° 40 (hoy E.E.T. N° 468) y del Ciclo Técnico (que funcionaba en el actual Colegio Nacional N° 2), dependientes ambos de la Comisión Nacional de Aprendizaje y Orientación Profesional. 
+          <br /><br />
+          El primer Secretario Técnico fue el Ing. Giordano Marchiori, quien conformó las primeras cátedras e inscribió los primeros alumnos en las carreras de: Ingeniería Mecánica, Ingeniería en Construcciones e Ingeniería Electromecánica.
+        </div>
+        <div class="col-md-6 col-sm-12 mt-3">
+          <img width="100%" src="https://lh3.googleusercontent.com/proxy/Byo_vLbkAauuscHEaowZPOmATP8uZYhbngYADgdtZEvi6yFFmghGskDek9qkVq9L1uBZe21A1hF0MSQiXBGRqvxefZutoYRVdGvxF8g4Tk78UOkOL1D1U9lqvUlN-FGMaOE">
+        </div>
+      </div>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1673.9289926023812!2d-60.64444439939346!3d-32.954758660072976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b7ab11d0eb49c3%3A0x11f1d3d54f950dd0!2sUniversidad%20Tecnol%C3%B3gica%20Nacional%20%7C%20Facultad%20Regional%20Rosario!5e0!3m2!1ses!2sar!4v1598106189927!5m2!1ses!2sar" width="100%" height="200" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
     </div>
-    <img style="max-width:300px;" src="https://lh3.googleusercontent.com/proxy/O2VoUkahej6Ccy_ORP85MW_HvttEpAnGfWxACGpadkA28SaGWKT9Qs2pfgj2hY0kQP26eAlh0VoWOx1EDoUU6PHuJHIhpFbSMH0JS4uMYOLIhsmsYlVprUq0CX1I4c0" alt="">
-  </div>
+    </div>
 </template>
 
 <script>
