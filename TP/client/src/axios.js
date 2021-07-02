d01b3da32b1c11155e4a3a8dea2a4b7ca02ab589
Gianfranco Raselli <66041020+GianfrancoRaselli@users.noreply.github.com>
2021-07-02 14:30:41 -0300

    modificacion url front

--- TP/client/src/axios.js
+++ TP/client/src/axios.js
@@ -1,3 +1,3 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost/Entornos Graficos/entornos-graficos-2021/TP/server/public/';
+axios.defaults.baseURL = 'https://utn-vacantes.herokuapp.com/';
