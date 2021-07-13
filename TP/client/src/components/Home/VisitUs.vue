a0ecd132fc11930ec4101133131469ccf75b7b67
Gianfranco Raselli <66041020+GianfrancoRaselli@users.noreply.github.com>
2021-07-13 16:41:07 -0300

    modificacion recursos

--- TP/client/src/components/Home/VisitUs.vue
+++ TP/client/src/components/Home/VisitUs.vue
@@ -1,57 +1,65 @@
 <template>
   <div class="visit-us-container">
     <div class="visit-description">
-      <h4>
-        Conocenos!
-      </h4>
+      <h4>Conocenos!</h4>
       <div class="mt-5 mb-5 px-3 row">
         <div class="col-md-6 col-sm-12 mt-3" style="text-align:justify">
-          En Rosario, la Facultad Regional de la Universidad Tecnológica Nacional tiene sus orígenes en 1953 sobre la base de la Escuela Fábrica N° 40 (hoy E.E.T. N° 468) y del Ciclo Técnico (que funcionaba en el actual Colegio Nacional N° 2), dependientes ambos de la Comisión Nacional de Aprendizaje y Orientación Profesional. 
-          <br /><br />
-          El primer Secretario Técnico fue el Ing. Giordano Marchiori, quien conformó las primeras cátedras e inscribió los primeros alumnos en las carreras de: Ingeniería Mecánica, Ingeniería en Construcciones e Ingeniería Electromecánica.
-          <br /><br />
+          En Rosario, la Facultad Regional de la Universidad Tecnológica Nacional tiene sus orígenes en 1953 sobre la base de la Escuela Fábrica N° 40 (hoy E.E.T. N° 468) y del Ciclo Técnico (que funcionaba en el actual Colegio Nacional N° 2), dependientes ambos de la Comisión Nacional de Aprendizaje y Orientación Profesional.
+          <br />
+          <br />El primer Secretario Técnico fue el Ing. Giordano Marchiori, quien conformó las primeras cátedras e inscribió los primeros alumnos en las carreras de: Ingeniería Mecánica, Ingeniería en Construcciones e Ingeniería Electromecánica.
+          <br />
+          <br />
           <b>MISION</b>
-          <p>Es MISIÓN de la Universidad Tecnológica Nacional: crear, preservar y transmitir los productos de los campos científicos, tecnológico y cultural para la formación plena del hombre como sujeto destinatario de esa cultura y de la técnica, extendiendo su accionar a la comunidad para contribuir a su desarrollo y transformación.
-          </p>
+          <p>Es MISIÓN de la Universidad Tecnológica Nacional: crear, preservar y transmitir los productos de los campos científicos, tecnológico y cultural para la formación plena del hombre como sujeto destinatario de esa cultura y de la técnica, extendiendo su accionar a la comunidad para contribuir a su desarrollo y transformación.</p>
           <br />
           <b>VISION</b>
-          <p>
-          <p>La Universidad Tecnológica Nacional ha sido concebida desde su comienzo como una Institución abierta a todos los hombres capaces de contribuir al proceso de desarrollo de la economía argentina, con clara conciencia de su compromiso con el bienestar y la justicia social, su respeto por la ciencia y la cultura, y la necesidad de su aporte al progreso de la Nación y las regiones que la componen, reivindicando los valores imprescriptibles de la libertad y la dignidad del pueblo argentino, y la integración armónica de los sectores sociales que la componen.
-          </p>
+          <p></p>
+          <p>La Universidad Tecnológica Nacional ha sido concebida desde su comienzo como una Institución abierta a todos los hombres capaces de contribuir al proceso de desarrollo de la economía argentina, con clara conciencia de su compromiso con el bienestar y la justicia social, su respeto por la ciencia y la cultura, y la necesidad de su aporte al progreso de la Nación y las regiones que la componen, reivindicando los valores imprescriptibles de la libertad y la dignidad del pueblo argentino, y la integración armónica de los sectores sociales que la componen.</p>
           <br />
           <b>VALORES</b>
-          <p>En este plan se priorizan los siguientes valores que sustentan la misión y la visión de la institución y que orientan sus lineamientos estratégicos y son marco para las distintas acciones: excelencia, ética, colaboración, inclusión, solidaridad, pertenencia, compromiso e innovación.
-          </p>
+          <p>En este plan se priorizan los siguientes valores que sustentan la misión y la visión de la institución y que orientan sus lineamientos estratégicos y son marco para las distintas acciones: excelencia, ética, colaboración, inclusión, solidaridad, pertenencia, compromiso e innovación.</p>
         </div>
         <div class="col-md-6 col-sm-12 mt-3 pl-5">
-          <img width="100%" src="utnfrente.jpg" alt="Foto de frente de las instalaciones de la UTN">
+          <img
+            width="100%"
+            src="/utnfrente.jpg"
+            alt="Foto de frente de las instalaciones de la UTN"
+          />
         </div>
       </div>
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1673.9289926023812!2d-60.64444439939346!3d-32.954758660072976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b7ab11d0eb49c3%3A0x11f1d3d54f950dd0!2sUniversidad%20Tecnol%C3%B3gica%20Nacional%20%7C%20Facultad%20Regional%20Rosario!5e0!3m2!1ses!2sar!4v1598106189927!5m2!1ses!2sar" width="100%" height="200" frameborder="0" style="border:0;" allowfullscreen="" alt="Mapa de ubicacion" aria-hidden="false" tabindex="0"></iframe>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1673.9289926023812!2d-60.64444439939346!3d-32.954758660072976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b7ab11d0eb49c3%3A0x11f1d3d54f950dd0!2sUniversidad%20Tecnol%C3%B3gica%20Nacional%20%7C%20Facultad%20Regional%20Rosario!5e0!3m2!1ses!2sar!4v1598106189927!5m2!1ses!2sar"
+        width="100%"
+        height="200"
+        frameborder="0"
+        style="border:0;"
+        allowfullscreen
+        alt="Mapa de ubicacion"
+        aria-hidden="false"
+        tabindex="0"
+      ></iframe>
     </div>
-    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-  .visit-us-container{
-    background-color: RGBA(0, 0, 0, .1);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 3rem;
-    margin-bottom:-2rem;
-  }
-  .visit-description{
-    margin-right: 1rem; 
-  }
-  @media(max-width:560px){
-  .visit-us-container{
+.visit-us-container {
+  background-color: RGBA(0, 0, 0, 0.1);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 3rem;
+  margin-bottom: -2rem;
+}
+.visit-description {
+  margin-right: 1rem;
+}
+@media (max-width: 560px) {
+  .visit-us-container {
     padding: 1rem;
   }
 }
